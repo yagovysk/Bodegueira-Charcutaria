9e21d28b1406f42083f88f44223b2ef953d68c87
yagovysk <yago09regis@gmail.com>
2025-04-07 10:23:12 -0300

    adicionei animação de texto sendo digitado e responsividade mobile

---
+++ script.js
@@ -0,0 +1,49 @@
+function typeWriter(elementId, text, speed, callback) {
+  const element = document.getElementById(elementId);
+
+  // Limpa o texto antes de começar
+  element.innerHTML = ""; // Limpa o conteúdo do elemento
+
+  let i = 0;
+
+  function type() {
+    if (i < text.length) {
+      element.innerHTML += text.charAt(i); // Insere a letra diretamente
+      i++;
+      setTimeout(type, speed);
+    } else {
+      // Chama o callback após a digitação
+      if (callback) {
+        setTimeout(callback, 1000);
+      }
+    }
+  }
+
+  type();
+}
+
+// Inicia o efeito de digitação para cada elemento
+document.addEventListener("DOMContentLoaded", () => {
+  typeWriter(
+    "typewriter-h1",
+    "Bem-vindo a Bodegueira & Charcutaria",
+    100,
+    () => {
+      typeWriter("typewriter-h2", "A melhor do Distrito Federal!", 100, () => {
+        typeWriter(
+          "typewriter-p",
+          "Os melhores produtos que uma bodegueira pode oferecer, não percam nossas melhores ofertas!",
+          100
+        );
+      });
+    }
+  );
+
+  // Alterna o menu mobile
+  const menuToggle = document.querySelector(".menu-toggle");
+  const navMobile = document.querySelector(".nav-mobile");
+
+  menuToggle.addEventListener("click", () => {
+    navMobile.classList.toggle("active");
+  });
+});
